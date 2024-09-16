93da2101d39c4771ad256c912806ca0d6388965a
Andre Kolodochka <59625655+andrekolodochka@users.noreply.github.com>
2024-09-16 01:28:06 +0000

    msft

--- update_share_price.py
+++ update_share_price.py
@@ -18,10 +18,13 @@ api_key = sys.argv[1]
 # Get the stock symbol from command line arguments
 stock_symbol = sys.argv[2]
 
-# make the API call to twelvedata to get the historicalclose day share price
-# save data to a file
+# make the API call to twelvedata to get the historical share price
+# save date and close price to a file
 # make file name in the format ${stock_symbol}.json
 url = f'https://api.twelvedata.com/time_series?symbol={stock_symbol}&interval=1day&start_date={start_date}&apikey={api_key}'
 response = requests.get(url)
 json_response = response.json()
 file_name = f'{stock_symbol}.json'
+with open(file_name, 'w') as file:
+    json.dump(json_response, file)
+
